920c8865eb1c1ecfffc294ee2927c3b7894a9d3d
DESKTOP <rotmzmfh@gmail.com>
2021-02-13 22:07:04 +0900

    feat : 1541번 풀이

---
+++ 1541_잃어버린 괄호/Baek1541.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<char> v;
+string s,num;
+
+void Solution()
+{
+    int result = 0;
+    num.clear();
+    char temp = '+';
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (i == v.size() - 1)
+        {
+            if (v[i] == '-' || v[i] == '+')
+            {
+                if (temp == '-')
+                    result -= stoi(num);
+                else
+                    result += stoi(num);
+            }
+            else
+            {
+                num += v[i];
+
+                if (temp == '-')
+                    result -= stoi(num);
+                else
+                    result += stoi(num);
+            }
+        }
+        if (v[i] == '-' || v[i] == '+')
+        {
+            if (temp == '-')
+                result -= stoi(num);
+            else
+                result += stoi(num);
+
+            num.clear();
+
+            if (v[i] == '-') temp = '-';
+        }
+        else
+        {
+            num += v[i];
+        }
+    }
+    cout << result << endl;
+}
+
+int main()
+{
+    cin >> s;
+    for (int i = 0; i < s.length(); i++)
+    {
+        v.push_back(s[i]);
+    }
+    Solution();
+}

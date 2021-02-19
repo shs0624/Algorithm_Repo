8a2fd8219c77063364e9cb3457b76ce9782c627d
DESKTOP <rotmzmfh@gmail.com>
2021-02-20 00:42:12 +0900

    feat: 2847번 풀이

---
+++ 2847_게임을 만든 동준이/Baek2847.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+vector<int> Exp;
+
+void Solution()
+{
+    int answer = 0;
+    int Prev;
+    for (int i = Exp.size() - 1; i >= 1; i--)
+    {
+        Prev = Exp[i - 1];
+        if (Exp[i] <= Prev)
+        {
+            int diff = Prev - (Exp[i] - 1);
+            Exp[i - 1] = Prev - diff;
+            answer += diff;
+        }
+    }
+
+    cout << answer << endl;
+}
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        Exp.push_back(num);
+    }
+    Solution();
+}

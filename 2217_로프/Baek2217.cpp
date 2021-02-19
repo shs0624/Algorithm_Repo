6d250a8f33763ffeb61c83c7af5cd53962b347ca
DESKTOP <rotmzmfh@gmail.com>
2021-02-19 22:25:54 +0900

    feat: 2217번 풀이

---
+++ 2217_로프/Baek2217.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N;
+vector<int> rope;
+
+void Solution()
+{
+    int Max = 0, cnt = 0, amount = 0, Min = 10001;
+
+    sort(rope.begin(), rope.end(), greater<>());
+    for (int i = 0; i < rope.size(); i++)
+    {
+        amount += rope[i];
+
+        Min = min(Min, rope[i]);
+        Max = max(Max, Min * (i + 1));
+    }
+
+    cout << Max << endl;
+}
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        rope.push_back(num);
+    }
+    Solution();
+}

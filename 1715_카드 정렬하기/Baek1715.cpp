437b9b7a4226a8d8cc695d3834ac8fb35715ee21
DESKTOP <rotmzmfh@gmail.com>
2021-02-21 19:57:14 +0900

    feat: 1715번 풀이

---
+++ 1715_카드 정렬하기/Baek1715.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, answer = 0;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        pq.push(num);
+    }
+
+    if (N == 1) answer = 0;
+    else
+    {
+        while (!pq.empty())
+        {
+            int a = pq.top(); pq.pop();
+            int b = pq.top(); pq.pop();
+
+            answer += a + b;
+            if (!pq.empty()) pq.push(a + b);
+        }
+    }
+
+    cout << answer << endl;
+}

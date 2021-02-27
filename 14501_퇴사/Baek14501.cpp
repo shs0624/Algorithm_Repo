5dd78181a5d436c0ec921d8bf37eb8735bc642b4
DESKTOP <rotmzmfh@gmail.com>
2021-02-27 16:14:44 +0900

    feat: 14501번 풀이

---
+++ 14501_퇴사/Baek14501.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, result;
+vector<pair<int, int>> v;
+
+//DFS가 맞는지는 모르겠음
+void DFS(int day, int ans)
+{
+    cout << "day : " << day << " ans : " << ans << endl;
+    if (day >= N)
+    {
+        result = max(result, ans);
+        return;
+    }
+
+    int a = v[day].second;
+    if (day + v[day].first - 1 < N)
+    {
+        DFS(day + v[day].first, ans + a);
+    }
+    if (v[day].first != 1) DFS(day + 1, ans);
+}
+
+int main()
+{
+    result = 0;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        v.push_back(make_pair(a, b));
+    }
+    DFS(0, 0);
+
+    cout << result << endl;
+}

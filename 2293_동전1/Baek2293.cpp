00033f4b9a7476f4a11407e6a8ddc398443c7c0f
DESKTOP <rotmzmfh@gmail.com>
2021-04-17 21:23:30 +0900

    feat: 2293번 풀이

---
+++ 2293_동전1/Baek2293.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, k;
+int dp[10001];
+int arr[101];
+
+void Solution()
+{
+    dp[0] = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = arr[i]; j <= k; j++)
+        {
+            dp[j] = dp[j] + dp[j - arr[i]];
+        }
+    }
+    cout << dp[k] << endl;
+}
+
+int main()
+{
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Solution();
+}

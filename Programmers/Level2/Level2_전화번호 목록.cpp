2da66ec2d8c5408f0c2b605af98f78f96325f9c1
DESKTOP <rotmzmfh@gmail.com>
2021-08-07 18:29:13 +0900

    feat : Level2 문제풀이

---
+++ Programmers/Level2/Level2_전화번호 목록.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+bool solution(vector<string> phone_book)
+{
+    bool answer = true;
+
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 1; i < phone_book.size(); i++)
+    {
+        for (int j = 0; j < phone_book[i - 1].size(); j++)
+        {
+            if (phone_book[i - 1][j] != phone_book[i][j]) break;
+            if (j == phone_book[i - 1].size() - 1) answer = false;
+        }
+
+        if (!answer) break;
+    }
+
+    return answer;
+}

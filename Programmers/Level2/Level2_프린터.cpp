b65ce31d59921f40a6c0c99a7433441c9beb68a9
DESKTOP <rotmzmfh@gmail.com>
2021-08-08 15:25:21 +0900

    feat: level2 문제풀이

---
+++ Programmers/Level2/Level2_프린터.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<pair<int, int>> q;
+    priority_queue<int> pq;
+
+    for (int i = 0; i < priorities.size(); i++)
+    {
+        q.push(make_pair(i, priorities[i]));
+        pq.push(priorities[i]);
+    }
+
+    while (!pq.empty())
+    {
+        int idx = q.front().first;
+        int value = q.front().second;
+        q.pop();
+
+        if (pq.top() == value)
+        {
+            answer++;
+            pq.pop();
+
+            if (idx == location)
+                break;
+        }
+        else
+        {
+            q.push(make_pair(idx, value));
+        }
+    }
+
+    return answer;
+}

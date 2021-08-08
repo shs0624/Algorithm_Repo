d1dc8aaea678bafccca1add72881c383946e373d
DESKTOP <rotmzmfh@gmail.com>
2021-08-08 12:25:11 +0900

    feat: level2 문제풀이

---
+++ Programmers/Level2/Level2_타겟 넘버.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+
+void dfs(vector<int> numbers, int target, int sum, int idx)
+{
+    if (idx == numbers.size())
+    {
+        if (sum == target) answer++;
+        return;
+    }
+
+    dfs(numbers, target, sum + numbers[idx], idx + 1);
+    dfs(numbers, target, sum - numbers[idx], idx + 1);
+}
+
+int solution(vector<int> numbers, int target) {
+    answer = 0;
+    dfs(numbers, target, 0, 0);
+    return answer;
+}
